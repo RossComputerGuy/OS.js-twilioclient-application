4693896db6ca46000725aff19b3764d8b34b2d81
Spaceboy Ross <spaceboyross@yandex.com>
2018-11-30 09:32:50 -0800

    Added keywords and locales

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ import {
 	Box,BoxContainer,Button,Menu,Menubar,MenubarItem,TextField,TextareaField,Toolbar
 } from "@osjs/gui";
 
-const createConnectDialog = (core,callback) => {
+const createConnectDialog = (core,_,callback) => {
 	core.make("osjs/dialogs").create({
 		buttons: ["ok","cancel"],
-		window: { title: "Login", dimension: { width: 400, height: 250 } }
+		window: { title: _("LOGIN_TITLE"), dimension: { width: 400, height: 250 } }
 	},dialog => null,(btn,value) => {
 		if(btn == "ok") return callback(value);
 	}).render(($content,dialogWindow,dialog) => {
@@ -26,14 +26,14 @@ const createConnectDialog = (core,callback) => {
 		(state,actions) => dialog.createView([
 			h(Box,{ grow: 1, padding: false },[
 				h(Box,{},[
-					h(BoxContainer, {},"Session ID"),
+					h(BoxContainer, {},_("LOGIN_SESSIONID")),
 					h(TextField,{
 						value: state.session_id,
 						oninput: (ev,value) => actions.setInput({ name: "session_id", value })
 					})
 				]),
 				h(Box,{},[
-					h(BoxContainer,{},"Token"),
+					h(BoxContainer,{},_("LOGIN_TOKEN")),
 					h(TextField,{ value: state.token, oninput: (ev,value) => actions.setInput({ name: "token", value })})
 				])
 			])
@@ -41,10 +41,10 @@ const createConnectDialog = (core,callback) => {
 	});
 };
 
-const createAboutDialog = core => {
+const createAboutDialog = (core,_) => {
 	core.make("osjs/dialogs").create({
 		buttons: ["ok"],
-		window: { title: "About", dimension: { width: 500, height: 200 } }
+		window: { title: _("ABOUT_TITLE"), dimension: { width: 500, height: 200 } }
 	},dialog => {
 		return dialog.app.getState();
 	},(btn,value) => {
@@ -58,19 +58,19 @@ const createAboutDialog = core => {
 		},
 		(state,actions) => dialog.createView([
 			h(Box,{ grow: 1, padding: false },[
-				h(BoxContainer,{},"Twilio Client - Version 1.0.0"),
-				h(BoxContainer,{},"Developed By Spaceboy Ross (https://youtube.com/c/SpaceboyRoss)"),
-				h(BoxContainer,{},"Licenced under Apache 2.0"),
-				h(BoxContainer,{},"This app is designed for sending messages for Twilio in OS.js V3, it provides a simple interface to use and requires no documentation.")
+				h(BoxContainer,{},_("ABOUT_VERSION")),
+				h(BoxContainer,{},_("ABOUT_DEV")),
+				h(BoxContainer,{},_("ABOUT_LICENSE")),
+				h(BoxContainer,{},_("ABOUT_DESCRIPTION"))
 			])
 		]),$content);
 	});
 };
 
-const createMessageDialog = (core,twilio) => {
+const createMessageDialog = (core,_,twilio) => {
 	core.make("osjs/dialogs").create({
 		buttons: ["ok","cancel"],
-		window: { title: "Send Message", dimension: { width: 400, height: 390 } }
+		window: { title: _("SEND_TITLE"), dimension: { width: 400, height: 390 } }
 	},dialog => {
 		return dialog.app.getState();
 	},(btn,value) => {
@@ -85,21 +85,21 @@ const createMessageDialog = (core,twilio) => {
 		(state,actions) => dialog.createView([
 			h(Box,{ grow: 1, padding: false },[
 				h(Box,{},[
-					h(BoxContainer, {},"From"),
+					h(BoxContainer, {},_("SEND_FROM")),
 					h(TextField,{
 						value: state.from,
 						oninput: (ev,value) => actions.setInput({ name: "from", value })
 					})
 				]),
 				h(Box,{},[
-					h(BoxContainer,{},"To"),
+					h(BoxContainer,{},_("SEND_TO")),
 					h(TextField,{
 						value: state.to,
 						oninput: (ev,value) => actions.setInput({ name: "to", value })
 					})
 				]),
 				h(Box,{},[
-					h(BoxContainer,{},"Message"),
+					h(BoxContainer,{},_("SEND_BODY")),
 					h(TextareaField,{
 						value: state.body,
 						oninput: (ev,value) => actions.setInput({ name: "body", value })
@@ -141,10 +141,10 @@ const createTwilio = async (core,proc,sid,token) => {
 	};
 };
 
-const createWindow = (core,proc,metadata,twilio) => {
+const createWindow = (core,_,proc,metadata,twilio) => {
 	proc.createWindow({
 		id: "TwilioClientWindow",
-		title: metadata.title.en_EN,
+		title: _("WIN_TITLE"),
 		dimension: { width: 400, height: 120 },
 		position: { left: 700, top: 200 }
 	}).on("destroy",() => proc.destroy()).render($content => {
@@ -154,31 +154,31 @@ const createWindow = (core,proc,metadata,twilio) => {
 				core.make("osjs/contextmenu").show({
 					position: ev.target,
 					menu: [
-						{ label: "Delete Session ID and Token", onclick: () => {
+						{ label: _("FILE_RESET"), onclick: () => {
 							delete proc.settings.sid;
 							delete proc.settings.token;
 							proc.saveSettings().then(() => {
 								proc.destroy();
 							});
 						} },
-						{ label: "Quit", onclick: () => proc.destroy() }
+						{ label: _("FILE_QUIT"), onclick: () => proc.destroy() }
 					]
 				});
 			},
 			menuAbout: ev => (state,actions) => {
-				createAboutDialog(core);
+				createAboutDialog(core,_);
 			},
 			newMessage: ev => (state,actions) => {
-				createMessageDialog(core,twilio);
+				createMessageDialog(core,_,twilio);
 			}
 		},
 		(state,actions) => h(Box,{ grow: 1, padding: false },[
 			h(Menubar,{},[
-				h(MenubarItem,{ onclick: ev => actions.menuFile(ev) },"File"),
-				h(MenubarItem,{ onclick: ev => actions.menuAbout(ev) },"About")
+				h(MenubarItem,{ onclick: ev => actions.menuFile(ev) },_("MENU_FILE")),
+				h(MenubarItem,{ onclick: ev => actions.menuAbout(ev) },_("MENU_ABOUT"))
 			]),
 			h(Box,{},[
-				h(Button,{ label: "New Message", onclick: ev => actions.newMessage(ev) })
+				h(Button,{ label: _("MESSAGE_CREATE"), onclick: ev => actions.newMessage(ev) })
 			])
 		]),$content);
 	});
@@ -186,9 +186,11 @@ const createWindow = (core,proc,metadata,twilio) => {
 
 const register = async (core,args,options,metadata) => {
 	const proc = core.make("osjs/application",{args,options,metadata});
+	const {translatable} = core.make("osjs/locale");
+	const _ = translatable(require("./locales.js"));
 	if(typeof(proc.settings.sid) == "undefined" || typeof(proc.settings.token) == "undefined") {
-		createConnectDialog(core,val => createWindow(core,proc,metadata,createTwilio(core,proc,val.session_id,val.token)));
-	} else createWindow(core,proc,metadata,createTwilio(core,proc,proc.settings.sid,proc.settings.token));
+		createConnectDialog(core,_,val => createWindow(core,_,proc,metadata,createTwilio(core,proc,val.session_id,val.token)));
+	} else createWindow(core,_,proc,metadata,createTwilio(core,proc,proc.settings.sid,proc.settings.token));
 	return proc;
 };
 
---
+++ locales.js
@@ -0,0 +1,30 @@
+const en_US = {
+	WIN_TITLE: "Twilio Client",
+	
+	MESSAGE_CREATE: "Create Message",
+	
+	MENU_FILE: "File",
+	
+	FILE_RESET: "Delete Session ID and Token",
+	FILE_QUIT: "Quit",
+	
+	MENU_ABOUT: "About",
+	
+	ABOUT_TITLE: "About",
+	
+	ABOUT_VERSION: "Twilio Client - Version 1.0.0",
+	ABOUT_DEV: "Developed By Spaceboy Ross (https://youtube.com/c/SpaceboyRoss)",
+	ABOUT_LICENSE: "Licenced under Apache 2.0",
+	ABOUT_DESCRIPTION: "This app is designed for sending messages for Twilio in OS.js V3, it provides a simple interface to use and requires no documentation.",
+	
+	SEND_TITLE: "Send Message",
+	SEND_FROM: "From",
+	SEND_TO: "To",
+	SEND_BODY: "Message",
+	
+	LOGIN_TITLE: "Login",
+	LOGIN_SESSIONID: "Session ID",
+	LOGIN_TOKEN: "Token"
+};
+
+module.exports = {en_US};
